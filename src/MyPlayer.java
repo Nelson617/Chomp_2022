6918120c42ba2adc09a0cf0dfd0c19580d6cd045
nelson <ralph_nelson25@milton.edu>
2024-12-18 09:06:37 -0500

    worked on during class

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -7,10 +7,24 @@ public class MyPlayer {
     public MyPlayer() {
         columns = new int[10];
 
+        int row = 3;
+        int col = 3;
+        int array[][] = new int [row][col];
+
         /***
          * This code will run just once, when the game opens.
          * Add your code here.
          */
+
+        //fill columns array with 0's
+        for (int i = 0; i  < columns.length; i++);{
+            for (int w = 0; w < columns.length; w++);{
+
+
+
+            }
+
+        }
     }
 
     public Point move(Chip[][] pBoard) {
@@ -29,9 +43,13 @@ public class MyPlayer {
          * Add your code to return the row and the column of the chip you want to take.
          * You'll be returning a data type called Point which consists of two integers.
          */
+        System.out.println("My player clicked " );
 
         Point myMove = new Point(row, column);
         return myMove;
     }
 
 }
+// add code to fill in the columns array
+//so that it represents the gameboard in number form
+//print columns array to show its working
